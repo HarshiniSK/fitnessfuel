54862ae64d6390304107b72c50946187c3db959d
HarshiniSK <harshini28102@gmail.com>
2021-10-31 23:10:49 +0530

    cors error fix

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ const queryClient = new QueryClient();
 
 const App = () => {
 
+  const config = {
+    headers: {'Access-Control-Allow-Origin': "*"}
+  }
+
   const fetchData = async() => {
-    const {data} = await axios.get("https://dev-api.fitnessfuel360.com/app/home")
+    const {data} = await axios.get("/home")
     return data
   }
   
